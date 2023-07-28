6df1afb4a14654f5941bf38252984b845e3cd264
André Guergolet <aguergolet@gmail.com>
2023-07-28 16:55:02 -0300

    Remove unused extensions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tlgCode import tlgCode
-import cv2
+
 # Exemplo de uso da classe
 generator = tlgCode.TLGCode()
 url = "https://web.telegram.org/k/#@HenriqueCaires"
@@ -10,4 +10,4 @@ if qr_code_matrix is not None:
     print('b')
     qr_code_image = generator.generate_image()
     qr_code_image.save('qrcode.png')
-    generator.generate_stl3('qrcode.stl')
+
